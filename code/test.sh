61c1b5bf1c25e81afe291c34063ebae2063487fc
juanfal <juan.falgueras@gmail.com>
2024-01-26 13:59:34 +0100

    shorter test

--- code/test.sh
+++ code/test.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # 
 
-./ae5.py 4PruebaTroph8 --numGen=2000 -p \
+./ae5.py 4PruebaTroph8 --numGen=20 \
  --verbose --NumberOfCells=6000 \
  --NumberOfRsrcsInEachCell=20 \
  --species="0;NumberOfItems=32000;Distribution=100r;1;NumberOfItems=32000;Distribution=100r;2;Distribution=100r;NumberOfItems=20;3;Distribution=100r"
